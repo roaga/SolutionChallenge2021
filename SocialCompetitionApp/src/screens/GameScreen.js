0325c7b10739454dfdca4d479a1135ce6f7279e5
Rohan Agarwal <ro.agarwal@hotmail.com>
2021-02-18 11:16:38 -0500

    Hardcode game UI

--- SocialCompetitionApp/src/screens/GameScreen.js
+++ SocialCompetitionApp/src/screens/GameScreen.js
@@ -1,11 +1,11 @@
 import React, {useRef, useState} from 'react'
-import {View, Text, StyleSheet, TextInput, TouchableOpacity, ImageBackground, ScrollView} from 'react-native'
+import {View, Text, StyleSheet, TextInput, TouchableOpacity, ImageBackground, ScrollView, FlatList} from 'react-native'
 import {StatusBar} from 'expo-status-bar';
 import * as Sharing from 'expo-sharing';
 import {Feather} from "@expo/vector-icons";
 import ViewShot from "react-native-view-shot";
 import DropDownPicker from 'react-native-dropdown-picker';
-import { StackedAreaChart, PieChart, StackedBarChart, ProgressCircle, YAxis, XAxis } from 'react-native-svg-charts'
+import { StackedBarChart, ProgressCircle, YAxis, XAxis } from 'react-native-svg-charts'
 
 import {uStyles, colors} from '../styles.js'
 
@@ -16,7 +16,28 @@ export default GameScreen = () => {
     const tempMilestonesData = {
         labels: ["Swim", "Bike", "Run"], // optional
         data: [0.4, 0.6, 0.8]
-      };
+    };
+
+    const tempPointsData = [
+        {
+            cause: "Environment",
+            Thoughts: 3840,
+            Volunteering: 1920,
+            Activism: 960,
+            Contribution: 400,
+            Awareness: 400,
+        },
+        {
+            cause: "Fitness",
+            Thoughts: 4140,
+            Volunteering: 190,
+            Activism: 960,
+            Contribution: 350,
+            Awareness: 400,
+        },
+    ]
+
+    const types = ['Thoughts', 'Volunteering', 'Activism', 'Contribution', 'Awareness'];
 
     const sharePost = async () => {
         view.current.capture().then(uri => {
@@ -28,11 +49,53 @@ export default GameScreen = () => {
         <View style={styles.container}>
             <ViewShot ref={view} style={{height: "100%"}}>
                 <ScrollView style={{marginTop: 98}} contentContainerStyle={{paddingBottom: 96}}>
-                    <View style={[uStyles.searchCard, {height: 300}]}>
+                    <View style={[uStyles.searchCard, {height: 384}]}>
                         <Text style={[uStyles.header, {marginTop: 4, color: colors.black, paddingBottom: 8}]}>Points</Text>
+
+                        <Text style={[uStyles.subheader, {alignSelf: "center", color: colors.black, marginVertical: 16}]}>Your top cause is {tempPointsData[0].cause}!</Text>
+
+                        <View style={{flexDirection: "row", marginBottom: 8}}>
+                            <YAxis
+                                data={tempPointsData}
+                                contentInset={{ left: 16, right: 16 }}
+                                style={{width: 10}}
+                                svg={{fill: colors.black, fontSize: 10}}
+                                formatLabel={(value) => value.cause}
+                                numberOfTicks={tempPointsData.length}
+                            />
+                            <StackedBarChart
+                                style={{height: 200, width: "90%"}}
+                                keys={types} 
+                                colors={[colors.dark, colors.primary, colors.white, colors.black]}
+                                data={tempPointsData}
+                                showGrid={false}
+                                contentInset={{ top: 8, bottom: 8 }}
+                                horizontal={true}
+                            />
+                        </View>
+                        <XAxis
+                            style={{ marginHorizontal: 10, width: "90%" }}
+                            data={[0, 1, 2, 3, 4]}
+                            formatLabel={(a, index) => (index * (tempPointsData[0].Thoughts + tempPointsData[0].Volunteering + tempPointsData[0].Activism + tempPointsData[0].Contribution + tempPointsData[0].Awareness) / [0, 1, 2, 3, 4].length)}
+                            contentInset={{ left: 16, right: 16 }}
+                            svg={{ fontSize: 10, fill: colors.black }}
+                        />                        
+
+                        <Text style={[uStyles.subheader, {alignSelf: "center", color: colors.black, marginTop: 16}]}>You earned {tempPointsData.map(item => (item.Thoughts + item.Volunteering + item.Activism + item.Contribution + item.Awareness)).reduce((a, b) => a + b)} points total!</Text>
+                    </View>
+
+                    <View style={[uStyles.searchCard, {height: 340}]}>
+                        <Text style={[uStyles.header, {marginTop: 4, color: colors.black, paddingBottom: 8}]}>Impact Facts</Text>
+
+                        <Feather name="plus-circle" color={colors.primary} size={32} style={{alignSelf: "center"}}/>
+                        <Text style={[uStyles.body, {alignSelf: "center", color: colors.black, marginTop: 16, marginHorizontal: 12, textAlign: "center"}]}>You made a post that engaged 1321 people on Environment!</Text>
+                        <Feather name="share" color={colors.primary} size={32} style={{alignSelf: "center", marginTop: 8}}/>
+                        <Text style={[uStyles.body, {alignSelf: "center", color: colors.black, marginTop: 16, marginHorizontal: 12, textAlign: "center"}]}>You shared a post representing 120 people on Fitness!</Text>
+                        <Feather name="message-circle" color={colors.primary} size={32} style={{alignSelf: "center", marginTop: 8}}/>
+                        <Text style={[uStyles.body, {alignSelf: "center", color: colors.black, marginTop: 16, marginHorizontal: 12, textAlign: "center"}]}>You made a comment on a post that engaged 149 people!</Text>
                     </View>
                     
-                    <View style={[uStyles.searchCard, {height: 800}]}>
+                    <View style={[uStyles.searchCard, {height: 640}]}>
                         <Text style={[uStyles.header, {marginTop: 4, color: colors.black, paddingBottom: 8}]}>Milestones</Text>
 
                         <View>
@@ -47,6 +110,9 @@ export default GameScreen = () => {
                             <ProgressCircle style={{ height: 150, marginTop: 16}} progress={0.6} progressColor={colors.primary}/>
                             <Text style={[uStyles.body,{color: colors.black, position: "absolute", top: "50%", alignSelf: "center"}]}>Milestone 3</Text>
                         </View>
+                        
+                        <Text style={[uStyles.body,{color: colors.black, alignSelf: "center", marginTop: 32}]}>Last completed milestone 4!</Text>
+                        <Text style={[uStyles.subheader, {alignSelf: "center", color: colors.black, marginTop: 16}]}>32 milestones already reached!</Text>
 
                     </View>
 
