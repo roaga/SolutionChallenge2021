bfe7f9813a3c95fd7d57620a21dec750fc1db562
Rohan Agarwal <ro.agarwal@hotmail.com>
2021-02-20 10:37:24 -0500

    Fix Y axis

--- SocialCompetitionApp/src/screens/GameScreen.js
+++ SocialCompetitionApp/src/screens/GameScreen.js
@@ -4,14 +4,12 @@ import {StatusBar} from 'expo-status-bar';
 import * as Sharing from 'expo-sharing';
 import {Feather} from "@expo/vector-icons";
 import ViewShot from "react-native-view-shot";
-import DropDownPicker from 'react-native-dropdown-picker';
 import { StackedBarChart, ProgressCircle, YAxis, XAxis } from 'react-native-svg-charts'
 
 import {uStyles, colors} from '../styles.js'
 
 export default GameScreen = () => {
     const view = useRef();
-    const [category, setCategory] = useState("foryou");
 
     const tempMilestonesData = {
         labels: ["Swim", "Bike", "Run"], // optional
@@ -49,20 +47,23 @@ export default GameScreen = () => {
         <View style={styles.container}>
             <ViewShot ref={view} style={{height: "100%"}}>
                 <ScrollView style={{marginTop: 98}} contentContainerStyle={{paddingBottom: 96}}>
-                    <View style={[uStyles.searchCard, {height: 384}]}>
+                    <View style={[uStyles.searchCard, {height: 324}]}>
                         <Text style={[uStyles.header, {marginTop: 4, color: colors.black, paddingBottom: 8}]}>Points</Text>
 
-                        <Text style={[uStyles.subheader, {alignSelf: "center", color: colors.black, marginVertical: 16}]}>Your top cause is {tempPointsData[0].cause}!</Text>
-
                         <View style={{flexDirection: "row", marginBottom: 8}}>
-                            <YAxis
+                            <FlatList
                                 data={tempPointsData}
-                                contentInset={{ left: 16, right: 16 }}
-                                style={{width: 10}}
-                                svg={{fill: colors.black, fontSize: 10}}
-                                formatLabel={(value) => value.cause}
-                                numberOfTicks={tempPointsData.length}
-                            />
+                                scrollEnabled={false}
+                                renderItem={({item}) => {
+                                    return (
+                                        <Text style={[uStyles.message, {color: colors.dark, fontSize: 8}]}>{item.cause}</Text>
+                                    );
+                                }}
+                                keyExtractor={(item) => item.cause}
+                                style={{flex: 1, height: 200}}
+                                contentContainerStyle={{flex: 1, justifyContent: "space-evenly"}}
+                                showsVerticalScrollIndicator={false}
+                            />    
                             <StackedBarChart
                                 style={{height: 200, width: "90%"}}
                                 keys={types} 
@@ -77,11 +78,10 @@ export default GameScreen = () => {
                             style={{ marginHorizontal: 10, width: "90%" }}
                             data={[0, 1, 2, 3, 4]}
                             formatLabel={(a, index) => (index * (tempPointsData[0].Thoughts + tempPointsData[0].Volunteering + tempPointsData[0].Activism + tempPointsData[0].Contribution + tempPointsData[0].Awareness) / [0, 1, 2, 3, 4].length)}
-                            contentInset={{ left: 16, right: 16 }}
+                            contentInset={{ left: 32, right: 16 }}
                             svg={{ fontSize: 10, fill: colors.black }}
-                        />                        
-
-                        <Text style={[uStyles.subheader, {alignSelf: "center", color: colors.black, marginTop: 16}]}>You earned {tempPointsData.map(item => (item.Thoughts + item.Volunteering + item.Activism + item.Contribution + item.Awareness)).reduce((a, b) => a + b)} points total!</Text>
+                        />                    
+                        <Text style={[uStyles.body, {alignSelf: "center", color: colors.black, marginTop: 16}]}>You earned {tempPointsData.map(item => (item.Thoughts + item.Volunteering + item.Activism + item.Contribution + item.Awareness)).reduce((a, b) => a + b)} points total!</Text>
                     </View>
 
                     <View style={[uStyles.searchCard, {height: 340}]}>
@@ -112,7 +112,7 @@ export default GameScreen = () => {
                         </View>
                         
                         <Text style={[uStyles.body,{color: colors.black, alignSelf: "center", marginTop: 32}]}>Last completed milestone 4!</Text>
-                        <Text style={[uStyles.subheader, {alignSelf: "center", color: colors.black, marginTop: 16}]}>32 milestones already reached!</Text>
+                        <Text style={[uStyles.body, {alignSelf: "center", color: colors.black, marginTop: 16}]}>32 milestones already reached!</Text>
 
                     </View>
 
@@ -127,27 +127,6 @@ export default GameScreen = () => {
 
             <View style={uStyles.topBar}>
                 <Text style={[uStyles.title, {color: colors.primary, textAlign: 'left', marginTop: 32}]}>Impact</Text>
-                <View style={{flexDirection: "row"}}>
-                    <DropDownPicker
-                        items={[
-                            {label: "For You", value: "foryou", icon: () => <Feather name="list" size={18} color={colors.primary}/>},
-                            {label: "Fitness", value: "fitness", icon: () => <Feather name="activity" size={18} color={colors.primary}/>},
-                            {label: "Environment", value: "environment", icon: () => <Feather name="globe" size={18} color={colors.primary}/>},
-                        ]}
-                        defaultValue={category}
-                        containerStyle={{height: 32, width: 160, marginTop: 32}}
-                        style={{backgroundColor: colors.black, color: colors.white, borderWidth: 0, flexDirection: "row-reverse"}}
-                        dropDownStyle={{backgroundColor: colors.black, borderWidth: 0, height: 512, borderBottomRightRadius: 10, borderBottomLeftRadius: 10}}
-                        itemStyle={{justifyContent: "flex-start", textAlign: "right"}}
-                        activeItemStyle={{backgroundColor: colors.primary, borderRadius: 10}}
-                        globalTextStyle={uStyles.body}
-                        onChangeItem={item => setCategory(item.value)}
-                        autoScrollToDefaultValue
-                        searchable
-                        searchablePlaceholder={"Search..."}
-                        searchableStyle={{borderRadius: 20}}
-                    />
-                </View>
             </View>
 
             <StatusBar style="light" />
