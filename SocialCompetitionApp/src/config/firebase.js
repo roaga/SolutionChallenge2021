0345ee729a728d70f8685833f68584d78758aef1
Rohan Agarwal <47861399+roaga@users.noreply.github.com>
2021-02-28 13:15:29 -0500

    Delete firebase.js

--- SocialCompetitionApp/src/config/firebase.js
+++
@@ -1,8 +0,0 @@
-export default {
-    apiKey: "AIzaSyCuPmbSJmsUUUSoZrQrjSlp7n5LZzQ4OZM",
-    authDomain: "socialcompetitionapp.firebaseapp.com",
-    projectId: "socialcompetitionapp",
-    storageBucket: "socialcompetitionapp.appspot.com",
-    messagingSenderId: "595008339469",
-    appId: "1:595008339469:web:c4487fec4dc2c58a53df44"
-}
