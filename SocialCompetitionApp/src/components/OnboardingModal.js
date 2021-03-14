43b9f404c0ee3caf9452b1dfe20de2a18371c12e
Hane Yie <47871955+hyhane24@users.noreply.github.com>
2021-03-14 10:17:41 -0400

    onboarding graphics added

--- SocialCompetitionApp/src/components/OnboardingModal.js
+++ SocialCompetitionApp/src/components/OnboardingModal.js
@@ -12,45 +12,45 @@ export default OnboardingModal = (props) => {
             pages={[
                 {
                     backgroundColor: colors.dark,
-                    image: <Image source={require('../../assets/defaultProfilePhoto.png')} />,
-                    title: 'Welcome to SCA',
+                    image: <Image source={require('../../assets/img/Solu.png')} />,
+                    title: 'Welcome to Solu',
                     subtitle: 'Making every tap count.\n\n Impact made tangible, fresh, and social.',
                 },
                 {
                     backgroundColor: colors.dark,
-                    image: <Image source={require('../../assets/defaultProfilePhoto.png')} />,
+                    image: <Image source={require('../../assets/img/OB2.png')} />,
                     title: 'Contribute to a Cause',
                     subtitle: 'Volunteered? Have a thought? Saw something funny?\n\nMake posts about any cause you care about.',
                 },
                 {
                     backgroundColor: colors.dark,
-                    image: <Image source={require('../../assets/defaultProfilePhoto.png')} />,
+                    image: <Image source={require('../../assets/img/OB3.png')} />,
                     title: 'Experience a Cause',
                     subtitle: 'See the impact people all over are creating in your feed.\n\n Filter by cause at the top.',
                 },
                 {
                     backgroundColor: colors.dark,
-                    image: <Image source={require('../../assets/defaultProfilePhoto.png')} />,
+                    image: <Image source={require('../../assets/img/OB4.png')} />,
                     title: 'Join a Cause',
                     subtitle: "Find and follow new causes and top contributors.",
                 },
                 {
                     backgroundColor: colors.dark,
-                    image: <Image source={require('../../assets/defaultProfilePhoto.png')} />,
+                    image: <Image source={require('../../assets/img/OB5.png')} />,
                     title: 'Track Your Impact',
                     subtitle: "Earn points and reach milestones for posting, commenting, sharing, and more.\n\nEven the little things matter, and we'll show you.",
                 },
                 {
                     backgroundColor: colors.dark,
-                    image: <Image source={require('../../assets/defaultProfilePhoto.png')} />,
+                    image: <Image source={require('../../assets/img/OB6.png')} />,
                     title: 'Spread the Impact',
                     subtitle: "Share posts and your points to show off the impact one person can have in a network.",
                 },
                 {
                     backgroundColor: colors.dark,
-                    image: <Image source={require('../../assets/defaultProfilePhoto.png')} />,
+                    image: <Image source={require('../../assets/img/Solu.png')} />,
                     title: 'Ready?',
-                    subtitle: "Time to jump into SCA.\n\n You can view this info again in your profile page.",
+                    subtitle: "Time to jump into Solu.\n\n You can view this info again in your profile page.",
                 },
             ]}
             titleStyles={{...uStyles.title, color: colors.primary, fontSize: 32}}
